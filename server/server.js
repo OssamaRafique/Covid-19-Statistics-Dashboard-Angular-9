8153c98023df8eb29d942558dfe9703176d9979b
srz2 <srz222@gmail.com>
2020-03-25 01:08:18 -0400

    Added parsing of USA States and storage of parsed information

--- server/server.js
+++ server/server.js
@@ -182,7 +182,101 @@ var getCountries = async () => {
     redis.set(keys.countries, string);
     console.log(`Updated all countries: ${result.length} countries`);
 };
+var getStatesUSA = async () => {
+  let response;
+  try {
+    response = await axios.get("https://www.worldometers.info/coronavirus/country/us/");
+    if (response.status !== 200) {
+      console.log("Error", response.status);
+    }
+  } catch (err) {
+    return null;
+  }
+  // to store parsed data
+  const result = [];
+  // get HTML and parse death rates
+  const html = cheerio.load(response.data);
+  const countriesTable = html("table#usa_table_countries_today");
+  const countriesTableCells = countriesTable
+    .children("tbody")
+    .children("tr")
+    .children("td");
+  // NOTE: this will change when table format change in website
+  const totalColumns = 7;
+  const stateColIndex = 0;
+  const casesColIndex = 1;
+  const todayCasesColIndex = 2;
+  const deathsColIndex = 3;
+  const todayDeathsColIndex = 4;
+  const activeColIndex = 5;
+  // const sourceColIndex = 6; // Not using
+  // minus totalColumns to skip last row, which is total
+  for (let i = 0; i < countriesTableCells.length - totalColumns; i += 1) {
+    const cell = countriesTableCells[i];
 
+    // get state
+    if (i % totalColumns === stateColIndex) {
+      let state =
+        cell.children[0].data ||
+        cell.children[0].children[0].data ||
+        // state name with link has another level
+        cell.children[0].children[0].children[0].data ||
+        cell.children[0].children[0].children[0].children[0].data ||
+        "";
+      state = state.trim();
+      if (state.length === 0) {
+        // parse with hyperlink
+        state = cell.children[0].next.children[0].data || "";
+      }
+      state = state.trim();
+      result.push({ state });
+    }
+    // get cases
+    if (i % totalColumns === casesColIndex) {
+      let cases = cell.children.length != 0 ? cell.children[0].data : "";
+      result[result.length - 1].cases = parseInt(
+        cases.trim().replace(/,/g, "") || "0",
+        10
+      );
+    }
+    // get today cases
+    if (i % totalColumns === todayCasesColIndex) {
+      let cases = cell.children.length != 0 ? cell.children[0].data : "";
+      result[result.length - 1].todayCases = parseInt(
+        cases.trim().replace(/,/g, "") || "0",
+        10
+      );
+    }
+    // get deaths
+    if (i % totalColumns === deathsColIndex) {
+      let deaths = cell.children.length != 0 ? cell.children[0].data : "";
+      result[result.length - 1].deaths = parseInt(
+        deaths.trim().replace(/,/g, "") || "0",
+        10
+      );
+    }
+    // get today deaths
+    if (i % totalColumns === todayDeathsColIndex) {
+      let deaths = cell.children.length != 0 ? cell.children[0].data : "";
+      result[result.length - 1].todayDeaths = parseInt(
+        deaths.trim().replace(/,/g, "") || "0",
+        10
+      );
+    }
+    // get active cases
+    if (i % totalColumns === activeColIndex) {
+      let cases = cell.children.length != 0 ? cell.children[0].data : "";
+      result[result.length - 1].activeCases = parseInt(
+        cases.trim().replace(/,/g, "") || "0",
+        10
+      );
+    }
+  }
+
+  const string = JSON.stringify(result);
+  redis.set(keys.usa_states, string);
+  console.log(`Updated all USA States: ${result.length} States`);
+};
 var getHistory = async () => {
   let casesResponse, deathsResponse, recResponse;
   const date = new Date();
@@ -242,6 +336,7 @@ var getHistory = async () => {
   console.log(`Updated JHU CSSE Timeline: ${result.length} locations in total`);
 }
 getCountries()
+getStatesUSA()
 getAll()
 getHistory()
 
